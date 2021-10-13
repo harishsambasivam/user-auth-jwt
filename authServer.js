362766d89d72088df203f018c54258740ff08a41
harishsambasivam <harishsambasivam@gmail.com>
2021-10-13 21:01:10 +0530

    added logout and signout of all devices

--- authServer.js
+++ authServer.js
@@ -12,7 +12,18 @@ authServer.listen(process.env.AUTH_SERVER_PORT, () => {
   console.log(`Server running on PORT ${process.env.AUTH_SERVER_PORT}`);
 });
 
-authServer.post("/signin", (req, res) => {
+// const redisLogger = function (req, res, next) {
+//   console.log(JSON.stringify(refreshTokens, null, 2));
+//   next();
+// };
+
+// authServer.use(redisLogger);
+
+authServer.get("/", (req, res) => {
+  res.send("server is up and running...");
+});
+
+authServer.post("/login", (req, res) => {
   let { userData } = req.body;
   const { userId } = userData;
 
@@ -35,4 +46,31 @@ authServer.post("/signin", (req, res) => {
       status: "error",
     });
   }
+});
+
+authServer.post("/logout", (req, res) => {
+  const { userData, refreshToken } = req.body;
+  const { userId } = userData;
+
+  if (refreshTokens[userId]) {
+    refreshTokens[userId] = refreshTokens[userId].filter(
+      (token) => token != refreshToken
+    );
+    console.log(refreshTokens[userId]);
+  }
+
+  res.status(200).json({
+    status: "success",
+  });
+});
+
+authServer.post("signOutOfAllDevices", (req, res) => {
+  const { userData } = req.body;
+  const { userId } = userData;
+  if (refreshTokens[userId]) {
+    delete refreshTokens[userId];
+  }
+  res.status(200).json({
+    status: "success",
+  });
 });

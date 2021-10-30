d2913be5c17c5eeb5640b5f2d1e38c58308ea425
harishsambasivam <harishsambasivam@gmail.com>
2021-10-30 18:15:01 +0530

    added tests for util functions

--- components/utils/utils.js
+++ components/utils/utils.js
@@ -2,8 +2,9 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const saltRounds = parseInt(process.env.BCRYPT_SALT_ROUNDS);
 
-module.exports.getAccessToken = (userdata) => {
-  return jwt.sign(userdata, process.env.ACCESS_TOKEN_SECRET, {
+module.exports.getAccessToken = (refreshToken) => {
+  const { data: userData, err } = this.verifyToken(refreshToken);
+  return jwt.sign(userData, process.env.ACCESS_TOKEN_SECRET, {
     expiresIn: process.env.JWT_TTL,
   });
 };
@@ -24,4 +25,27 @@ module.exports.hashPassword = async (password) => {
 module.exports.verifyPassword = async (plainTextPassword, hashedPassword) => {
   const result = await bcrypt.compare(plainTextPassword, hashedPassword);
   return result === true;
+};
+
+module.exports.verifyToken = (token, type) => {
+  let data,
+    err = null;
+  try {
+    const secret =
+      type == "accessToken"
+        ? process.env.ACCESS_TOKEN_SECRET
+        : process.env.REFRESH_TOKEN_SECRET;
+    data = jwt.verify(token, secret);
+  } catch (e) {
+    if (e) err = e;
+  }
+  return { data, err };
+};
+
+module.exports.getTimeDiffInMinutes = (epoch1, epoch2) => {
+  var startTime = new Date(epoch1);
+  var endTime = new Date(epoch2);
+  var difference = endTime.getTime() - startTime.getTime(); // This will give difference in milliseconds
+  var resultInMinutes = Math.round(difference / 60000);
+  return resultInMinutes;
 };
--- components/utils/utils.test.js
+++ components/utils/utils.test.js
@@ -3,6 +3,8 @@ const {
   getRefreshToken,
   hashPassword,
   verifyPassword,
+  verifyToken,
+  getTimeDiffInMinutes,
 } = require("./utils");
 
 test("hash & verify the password", async () => {
@@ -12,12 +14,57 @@ test("hash & verify the password", async () => {
   expect(await verifyPassword(plainTextPassword, hashedPassword)).toBeTruthy;
 });
 
-test("create refresh token", async () => {
+test("create & verify refresh token", async () => {
   const userData = {
     userName: "john doe",
     userId: "0xabcd",
   };
 
-  const refreshToken = await getRefreshToken(userData);
+  // create refresh token & verify
+  const refreshToken = getRefreshToken(userData);
   expect(refreshToken.length).toBeGreaterThan(0);
+  const { data: decodedUserData } = verifyToken(refreshToken, "refreshToken");
+  expect(decodedUserData.userId).toBe(userData.userId);
+
+  // tamper the jwt & it should throw error
+  const tamperedToken = refreshToken.split("").push("a").toString();
+  const { err } = verifyToken(tamperedToken, "refreshToken");
+  expect(err).not.toBeNull;
+});
+
+test("time difference between now and (5 mins + now) is greater than 4  minutes", () => {
+  let date1 = Date.now();
+  // adding 5 mins to current time
+  let date2 = Date.now() + 5 * 60 * 1000;
+
+  expect(getTimeDiffInMinutes(date1, date2)).toBeGreaterThan(4);
+});
+
+test("create & verify the access token", () => {
+  const userData = {
+    userName: "john doe",
+    userId: "0xabcd",
+  };
+
+  // create refresh token
+  const refreshToken = getRefreshToken(userData);
+
+  // create access token with refresh token
+  const accessToken = getAccessToken(refreshToken);
+  expect(accessToken.length).toBeGreaterThan(0);
+
+  // validate access token payload by decoding it
+  const { data: decodedUserData } = verifyToken(accessToken, "accessToken");
+  expect(decodedUserData.userId).toBe(userData.userId);
+  expect(decodedUserData.userName).toBe(userData.userName);
+
+  // time diff between current time and jwt ttl should be > 55 mins
+  expect(
+    getTimeDiffInMinutes(Date.now(), decodedUserData.exp * 1000)
+  ).toBeGreaterThan(55);
+
+  // tamper access token & verify
+  const tamperedAccessToken = accessToken.split("").unshift("a").toString();
+  const { err } = verifyToken(tamperedAccessToken, "accessToken");
+  expect(err).not.toBeNull();
 });
